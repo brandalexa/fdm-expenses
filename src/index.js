3e5bb1596e60fe3ab036e5d503bce71f3be94d31
Brandon Alexander <brandonralexander@gmail.com>
2023-04-13 01:19:18 +0100

    Reverted index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login } from "./Login";
-import Home from "./Home";
-import { Register } from "./Register";
-import { Claim } from "./Claim";
+import reportWebVitals from "./reportWebVitals";
+import { ProSidebarProvider } from "react-pro-sidebar";
+
+import ViewUnsentClaims from "./pages/view-claims";
+import SideNav from "./components/sidenav";
+import Notifications from "./pages/notifications";
+import MyDetails from "./pages/my-details";
+import ChangePassword from "./pages/change-password";
+import Claim from "./pages/new-claim";
+import ManageClaims from "./pages/manage-pending-claims";
+import ViewClosedClaims from "./pages/view-closed-claims";
+import NewUser from "./pages/new-user";
+import ManageUser from "./pages/manage-user";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="home" element={<Home />} />
-        <Route path="claim" element={<Claim />} />
-      </Routes>
+    <ProSidebarProvider>
+      <App name="Brandon Alexander" notifications="4"/>
+    </ProSidebarProvider>
     </BrowserRouter>
-    {/* <App /> */}
-  </React.StrictMode>
 );
 
-//reportWebVitals(console.log);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
